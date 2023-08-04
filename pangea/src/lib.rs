4adfbe15c886925ac181754b1a586b198b455b47
Fawad Shaikh <fawad@candle.dev>
2023-08-03 22:46:20 -0400

    pangea working cli

--- pangea/src/lib.rs
+++ pangea/src/lib.rs
@@ -16,18 +16,11 @@ impl GeolocateOperation for Component {
         mut outputs: Self::Outputs,
         ctx: Context<Self::Config>,
     ) -> Result<(), Self::Error> {
-        //import the config "with" variables
-        let config = ctx.root_config();
-
         while let Some(ip) = ip.next().await {
             let ip = propagate_if_error!(ip, outputs, continue);
 
-            println!("ip: {}", ip);
-
-            let (mut get_geolocate_response, mut get_geolocate_response_body) = ctx
-                .provided()
-                .pangea_api
-                .ip_geolocate(once(ip), once(config.token.clone()))?;
+            let (mut get_geolocate_response, mut get_geolocate_response_body) =
+                ctx.provided().pangea_api.ip_geolocate(once(ip))?;
 
             while let (Some(get_geolocate_response), Some(get_geolocate_response_body)) = (
                 get_geolocate_response.next().await,
@@ -35,9 +28,6 @@ impl GeolocateOperation for Component {
             ) {
                 let response = propagate_if_error!(get_geolocate_response, outputs, continue);
                 let body = propagate_if_error!(get_geolocate_response_body, outputs, continue);
-
-                println!("response: {:?}", response);
-                println!("body: {:?}", body);
 
                 if response.status != types::http::StatusCode::Ok {
                     outputs.status.error(format!(
